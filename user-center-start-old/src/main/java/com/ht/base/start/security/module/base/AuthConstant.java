d1040defe03c99afe003f9b4d5a90a0b557b1132
jbzm <125964590@qq.com>
2019-01-08 11:11:34 +0800

    修改包结构

--- user-center-start-old/src/main/java/com/ht/base/start/security/module/base/AuthConstant.java
+++
@@ -1,9 +0,0 @@
-package com.ht.base.start.security.module.base;
-
-/**
- * @author zhengyi
- * @date 11/16/18 4:04 PM
- **/
-public class AuthConstant {
-    public final static String ROLE = "admin";
-}
